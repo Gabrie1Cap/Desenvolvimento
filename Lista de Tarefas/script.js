c3219bd2a08c4bbd4f0a049892a11657151a119c
Gabriel Flores Cardoso <126208572+Gabrie1Cap@users.noreply.github.com>
2025-06-30 20:13:59 -0300

    att lista de tarefas
    Adicionado um array e uma nova função com for que percorre os itens do array e os exibe na tela para o usuário

--- Lista de Tarefas/script.js
+++ Lista de Tarefas/script.js
@@ -1,19 +1,30 @@
+let tarefas = []
+
 function addTarefa(){
-    let inputTarefa = document.getElementById('inputTarefa')
-    let tarefa = inputTarefa.value;
+
+    const inputTarefa = document.getElementById('inputTarefa')
+    let tarefa = inputTarefa.value.trim();
+    const mensagem = document.getElementById('mensagem');
+    let mensagemCerta = 'Tarefa adicionada com sucesso!';
+    let mensagemErro = 'Por favor digite uma tarefa!';
 
     if(tarefa == ''){
-        alert('Por favor digite uma tarefa!')
+        mensagem.style.color = '#AD3204'
+        mensagem.textContent = mensagemErro;    
     }else{
-        
-    let listaTarefas = document.getElementById('listaTarefas');
-    let novaTarefa = document.createElement('li');
-
-    novaTarefa.textContent = tarefa;
-    listaTarefas.appendChild(novaTarefa);
-
-    let mensagem = document.getElementById('mensagem');
-    mensagem.textContent = 'Tarefa adicionada com sucesso!';
-
-    inputTarefa.value = '';
+        tarefas.push(tarefa)
+        mensagem.style.color = '#5BBA85'
+        mensagem.textContent = mensagemCerta;
+        renderList()
+        inputTarefa.value = '';
 }};
+
+    function renderList(){
+        const listaTarefas = document.getElementById('listaTarefas');
+        listaTarefas.innerHTML = ''
+        let i = 0
+        for(i ; i < tarefas.length ; i++){
+        const novaTarefa = document.createElement('li');
+        novaTarefa.textContent = tarefas[i];
+        listaTarefas.appendChild(novaTarefa,);
+}}
